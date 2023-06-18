eb43adabd5931c6408a92287da72cdae8c15b159
ilNubis <97183739+ilNubis@users.noreply.github.com>
2023-06-18 17:50:53 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Rule34',
+    version='0.0.1',
+    author='Nubis',
+    author_email='Nubis@hotmail.com',
+    description='Python package that allows you to access the content from the famous adult website',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ilNubis/Rule34',
+    project_urls = {
+        "Bug Tracker": "https://github.com/ilNubis/Rule34/issues"
+    },
+    license='MIT',
+    packages=['Rule34'],
+    install_requires=['requests', 'lxml'],
+)
